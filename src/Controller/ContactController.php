23568b28a543cc4cb88a5a58a780c262181bd378
hassanhaddane <74867946+hassanhaddane@users.noreply.github.com>
2021-11-15 16:14:42 +0100

    Add initial set of files

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,17 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/contactez-moi/{city}", name="contact")
      */
-    public function index(): Response
+    public function index(Request $request, string $city = ""): Response
     {
+        $name = $request->query->get('name');
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'controller_name' => 'Contrôleur de Contact',
+            'city' => $city,
+            'name' => $name
         ]);
     }
 }
